0b5843e4255954d5a6859e5e53e5f946fa984ea8
Myeongjin <mangs2e@naver.com>
2024-09-26 23:26:55 +0900

    [Feat] Navbar 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar bg="secondary" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
